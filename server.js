d95fcd66c7210bd3bf88bad342ddff014ff9deae
Israel-Weiss <yisraelv@gmail.com>
2023-07-19 20:27:59 +0300

    fix syntax err

--- server.js
+++ server.js
@@ -21,11 +21,16 @@ setupSocketAPI(http)
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.resolve(__dirname, 'public')))
 } else {
-    const corsOptions = {
-        origin: ['http://127.0.0.1:5173', 'http://127.0.0.1:8080', 'http://localhost:8080', 'http://127.0.0.1:3000', 'http://localhost:3000'],
-        credentials: true
-    }
-    app.use(cors(corsOptions))
+
+    app.use(express.static(path.resolve(__dirname, 'public')))
+
+
+    // const corsOptions = {
+    //     origin: ['http://127.0.0.1:5173', 'http://127.0.0.1:8080', 'http://localhost:8080', 'http://127.0.0.1:3000', 'http://localhost:3000'],
+    //     credentials: true
+    // }
+    // app.use(cors(corsOptions))
+
 }
 
 tempService.startTempInterval()
@@ -43,6 +48,5 @@ app.get('/**', (req, res) => {
 })
 
 http.listen(port, () => console.log('Server is running on port: ' + port))
-
 
 
