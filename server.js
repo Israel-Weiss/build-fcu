85ff0a496ee17a483ad0dfdc8c33cda1004cc3f0
Israel-Weiss <yisraelv@gmail.com>
2023-07-20 18:13:11 +0300

    add spinner icon

--- server.js
+++ server.js
@@ -24,13 +24,13 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.resolve(__dirname, 'public')))
 } else {
 
-    // app.use(express.static(path.resolve(__dirname, 'public')))
+    app.use(express.static(path.resolve(__dirname, 'public')))
 
-    const corsOptions = {
-        origin: ['http://127.0.0.1:5173', 'http://127.0.0.1:8080', 'http://localhost:8080', 'http://127.0.0.1:3000', 'http://localhost:3000'],
-        credentials: true
-    }
-    app.use(cors(corsOptions))
+    // const corsOptions = {
+    //     origin: ['http://127.0.0.1:5173', 'http://127.0.0.1:8080', 'http://localhost:8080', 'http://127.0.0.1:3000', 'http://localhost:3000'],
+    //     credentials: true
+    // }
+    // app.use(cors(corsOptions))
 }
 
 tempService.startTempInterval()
